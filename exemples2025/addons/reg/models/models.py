006506531aa25e761abfd599b1494c16b32e2290
xxjcaxx <jocasal@gmail.com>
2026-01-09 18:58:02 +0100

    wizard assistent

--- exemples2025/addons/reg/models/models.py
+++ exemples2025/addons/reg/models/models.py
@@ -25,15 +25,54 @@ class valvula_wizard(models.TransientModel):
 
     name = fields.Char()
     caval = fields.Float()
-    soci = fields.Many2one('res.partner')
+    #soci = fields.Many2one('res.partner', default = lambda v: v._context.get('soci_context'))
+    soci = fields.Many2one('res.partner', default = lambda v: v._context.get('active_id'))
+    state = fields.Selection([
+        ('valvula', "Valve data"),
+        ('soci', "Select Soci"),  
+        ('resume', "Resume")                                                                      
+      ], default='valvula')
+
+
     #servics = fields.One2many('reg.servici', 'valvula')
 
     def create_valve(self):
+        print(self.env.context.get('phone_context'))
         self.env['reg.valvula'].create({
             "name": self.name,
             "caval": self.caval,
             "soci": self.soci.id
         })
+
+    def next(self):
+        if self.state == 'valvula':
+            self.state = 'soci'
+        elif self.state == 'soci':
+            self.state = 'resume'
+
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': self._name,
+            'res_id': self.id,
+            'view_mode': 'form',
+            'target': 'new',
+        }
+
+
+    def previous(self):
+        if self.state == 'resume':
+            self.state = 'soci'
+        elif self.state == 'soci':
+            self.state = 'valvula'
+
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': self._name,
+            'res_id': self.id,
+            'view_mode': 'form',
+            'target': 'new',
+        }
+
 
 class servici(models.Model):
     _name = 'reg.servici'
