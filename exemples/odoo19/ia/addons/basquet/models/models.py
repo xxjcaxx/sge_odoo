f99ed5f599bf12cf50c862834ad36dc2acfe3a3e
xxjcaxx <jocasal@gmail.com>
2026-04-22 19:03:01 +0200

    exemples

--- exemples/odoo19/ia/addons/basquet/models/models.py
+++ exemples/odoo19/ia/addons/basquet/models/models.py
@@ -1,4 +1,6 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+import random
 
 
 class equip(models.Model):
@@ -12,15 +14,28 @@ class equip(models.Model):
     equips_rivals_ids = fields.Many2many('basquet.equip', 'basquet_equip_rival_rel', 'equip_id', 'rival_id', string='Equips rivals')
     equips_agermanats_ids = fields.Many2many('basquet.equip', 'basquet_equip_agermanat_rel', 'equip_id', 'agermanat_id', string='Equips agermanats')
 
+    @api.constrains('equips_agermanats_ids')
+    def _check_agermanats(self):
+        for e in self:
+            if len(e.equips_agermanats_ids) > 4:
+                raise ValidationError("Too many agermanats %s" % len(e.equips_agermanats_ids))
 
 class jugador(models.Model):
     _name = 'basquet.jugador'
     _description = 'basquet.jugador'
 
     name = fields.Char()
-    equip_id = fields.Many2one('basquet.equip', string='Equip', domain="[('estadi_id', '!=', False)]")
+    def _get_random_team(self):
+        aleatori = random.randint(0,len(self.env['basquet.equip'].search([]))-1)
+        return self.env['basquet.equip'].search([]).ids[aleatori]
+    
+    equip_id = fields.Many2one('basquet.equip', string='Equip', 
+                               domain="[('estadi_id', '!=', False)]", default= lambda self: self.env['basquet.equip'].search([]).ids[random.randint(0,len(self.env['basquet.equip'].search([]))-1)])
     estadi_nom = fields.Char(string='Pavellò', related='equip_id.estadi_id.name')
-    es_capita = fields.Boolean(string='És capità')
+    es_capita = fields.Boolean(string='És capità', default=False)
+    triples = fields.Float(default = lambda self: random.random()*100)
+    ptl = fields.Float(default = lambda self: random.random()*100)
+    foto = fields.Image(max_width=100, max_height=100)
 
 
 class pavello(models.Model):
