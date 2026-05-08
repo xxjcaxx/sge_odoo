bc3ea61534064e4655906fc9837e56946bc4a1db
xxjcaxx <jocasal@gmail.com>
2026-05-08 18:53:39 +0200

    orm

--- exemples/odoo19/ia/addons/basquet/models/models.py
+++ exemples/odoo19/ia/addons/basquet/models/models.py
@@ -15,12 +15,36 @@ class equip(models.Model):
     equips_rivals_ids = fields.Many2many('res.partner', 'basquet_equip_rival_rel', 'equip_id', 'rival_id', string='Equips rivals')
     equips_agermanats_ids = fields.Many2many('res.partner', 'basquet_equip_agermanat_rel', 'equip_id', 'agermanat_id', string='Equips agermanats')
     is_equip = fields.Boolean()
+    triples_average = fields.Float(compute='_get_triples_average')
+    triples_average_mvp = fields.Float(compute='_get_triples_average')
+    mvps = fields.Many2many('basquet.jugador',compute='_get_triples_average')
+    lvps = fields.Many2many('basquet.jugador',compute='_get_triples_average')
 
     @api.constrains('equips_agermanats_ids')
     def _check_agermanats(self):
         for e in self:
             if len(e.equips_agermanats_ids) > 4:
                 raise ValidationError("Too many agermanats %s" % len(e.equips_agermanats_ids))
+            
+
+    def generar_jugadors(self):
+        for i in range(5):
+             self.env['basquet.jugador'].create({
+                 "name": str(i)+"name", 
+                 "equip_id": self.id,
+                 "triples": random.randint(0,100),
+                 "ptl": random.randint(0,100),
+                 })
+             
+    @api.depends('jugador_ids')
+    def _get_triples_average(self):
+        for e in self:
+            e.triples_average = sum(e.jugador_ids.mapped('triples'))/len(e.jugador_ids)
+            mvps = e.jugador_ids.filtered(lambda j: j.ptl > 50 and j.triples > 50)
+            e.triples_average_mvp = sum(mvps.mapped('triples'))/len(mvps)
+
+            e.mvps = mvps.ids
+            e.lvps = (e.jugador_ids - e.mvps).ids
 
 class jugador(models.Model):
     _name = 'basquet.jugador'
@@ -38,6 +62,15 @@ class jugador(models.Model):
     triples = fields.Float(default = lambda self: random.random()*100)
     ptl = fields.Float(default = lambda self: random.random()*100)
     foto = fields.Image(max_width=100, max_height=100)
+
+    def change_capita(self):
+        self.es_capita = not self.es_capita
+
+
+    def buscar_pareguts(self):
+        triples = self.triples
+        iguals = self.search([('triples','>',triples - 2),('triples','<',triples + 2)])
+        print(iguals)
 
 
 class pavello(models.Model):
