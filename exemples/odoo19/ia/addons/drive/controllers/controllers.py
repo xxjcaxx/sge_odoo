a24f1dc602ef820c12653fba0ea9767509fb1d5a
xxjcaxx <xxjcaxx@yahoo.es>
2026-06-16 15:00:59 +0200

    solucio

--- exemples/odoo19/ia/addons/drive/controllers/controllers.py
+++ exemples/odoo19/ia/addons/drive/controllers/controllers.py
@@ -1,21 +1,35 @@
-# from odoo import http
+import json
+
+from odoo import http
+from odoo.http import request
 
 
-# class Drive(http.Controller):
-#     @http.route('/drive/drive', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class Drive(http.Controller):
+     @http.route('/drive/files', auth='public', type='http', methods=['GET'], csrf=False, cors='*')
+     def index(self, **kw):
+          
+          criterio = (kw.get('q') or kw.get('search') or '').strip()
+          domain = []
+          if criterio:
+               domain = [('name', 'ilike', criterio)]
 
-#     @http.route('/drive/drive/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('drive.listing', {
-#             'root': '/drive/drive',
-#             'objects': http.request.env['drive.drive'].search([]),
-#         })
+          files = request.env['drive.file'].sudo().search(domain, order='name asc')
+          payload = [
+               {
+                    'id': file.id,
+                    'name': file.name,
+               }
+               for file in files
+          ]
 
-#     @http.route('/drive/drive/objects/<model("drive.drive"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('drive.object', {
-#             'object': obj
-#         })
+          body = json.dumps(
+               {
+                    'count': len(payload),
+                    'query': criterio,
+                    'results': payload,
+               },
+               ensure_ascii=False,
+          )
+          
+          return request.make_response(body)
 
--- exemples/odoo19/ia/addons/drive/models/models.py
+++ exemples/odoo19/ia/addons/drive/models/models.py
@@ -7,6 +7,39 @@ class drive(models.Model):
 
     name = fields.Char()
     file = fields.Binary()
-    user = fields.Many2one('res.users')
-    users = fields.Many2many('res.users', string='Users with access')
+    user = fields.Many2one('res.users', default=lambda self: self.env.user)
+    interested = fields.Many2many('res.users')
 
+    def toggle_interested(self):
+        if self.env.user in self.interested:
+            self.interested = [(3, self.env.user.id)]
+        else:
+            self.interested = [(4, self.env.user.id)]
+
+
+class res_users(models.Model):
+    _inherit = 'res.users'
+
+    drive_file_ids = fields.Many2many('drive.file', compute='_get_drive_files')
+
+    
+    def _get_drive_files(self):
+        for user in self:
+            interested = self.env['drive.file'].search([('interested', 'in', user.id)])
+            owner = self.env['drive.file'].search([('user', '=', user.id)])
+            user.drive_file_ids = interested | owner
+
+
+class file_wizard(models.TransientModel):
+    _name = 'drive.wizard'
+    _description = 'File Wizard'
+
+    name = fields.Char()
+    file = fields.Binary()
+
+    def create_file(self):
+        self.env['drive.file'].create({
+            'name': self.name,
+            'file': self.file,
+            'user': self.env.user.id
+        })
